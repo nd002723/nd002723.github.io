8303e7912c29cd7d14cb72e3650536b38620a1c9
DESKTOP-ROQQESA\Shino <zhazhawsy@gmail.com>
2018-06-11 11:43:12 +0800

    homepage
    Signed-off-by: DESKTOP-ROQQESA\Shino <zhazhawsy@gmail.com>

---
+++ js/404.js
@@ -0,0 +1,6 @@
+document.onkeydown = function(event){if (
+                    (event.keyCode == 123)) //屏蔽 F12
+                    {
+                    return false;
+                }}
+            window.onhelp = function(){return false;}
